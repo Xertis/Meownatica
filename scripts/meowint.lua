bec3d7d015e9c0027b2a43a87fed92757d1df3d8
Xertis <xertis3785@gmail.com>
2024-07-19 21:54:20 +0300

    bug fix

--- scripts/meowint.lua
+++ scripts/meowint.lua
@@ -6,16 +6,17 @@ local reader = require 'meownatica:tools/read_toml'
 local num_file = 0
 
 local function printStructures(tbl)
-    num_file = num_file + 1
-    while file.exists(reader.sys_get('savepath') .. 'save_meownatic_' .. num_file .. reader.sys_get('fileformat')) do
+    if #tbl > 0 then
         num_file = num_file + 1
+        while file.exists(reader.sys_get('savepath') .. 'save_meownatic_' .. num_file .. reader.sys_get('fileformat')) do
+            num_file = num_file + 1
+        end
+        print(lang.get('Save Meownatic'))
+        
+        local save_table = save_u.convert_save(tbl)
+        save_u.write(save_table, reader.sys_get('savepath') .. 'save_meownatic_' .. num_file .. reader.sys_get('fileformat'))
+        meow_schem.save_to_config('save_meownatic_' .. num_file .. reader.sys_get('fileformat'))
     end
-    print(lang.get('Save Meownatic'))
-
-    local save_table = save_u.convert_save(tbl)
-
-    save_u.write(save_table, reader.sys_get('savepath') .. 'save_meownatic_' .. num_file .. reader.sys_get('fileformat'))
-    meow_schem.save_to_config('save_meownatic_' .. num_file .. reader.sys_get('fileformat'))
 end
   
 function on_use_on_block(x, y, z, playerid)
