b308e1535f4b00eb4e96b81ffaaf397735c75342
Xertis <xertis3785@gmail.com>
2024-07-27 13:43:55 +0300

    bug fix

--- modules/schematics_editors/change_schem.lua
+++ modules/schematics_editors/change_schem.lua
@@ -3,16 +3,16 @@ local save_u = require 'meownatica:tools/save_utils'
 local container = require 'meownatica:container_class'
 local meow_schem = require 'meownatica:schematics_editors/SchemEditor'
 local meow_change = { }
-local point2 = 2
+local meow_point = 2
 
 local FORMAT = reader.sys_get('fileformat')
 
 function meow_change.convert_schem(meownatic_load)
     local source = meownatic_load:match("(.+)%..+") .. FORMAT
-    local source1 = reader.sys_get('savepath') .. source
+    local source = reader.sys_get('savepath') .. source
     local is_convert, reason = meow_schem.convert(meownatic_load, FORMAT, meownatic_load)
     if is_convert then
-        local doc = save_u.read(source1)
+        local doc = save_u.read(source)
         container.send_g(save_u.convert_read(doc))
         return container.get_g()
     else
@@ -20,55 +20,50 @@ function meow_change.convert_schem(meownatic_load)
     end
 end
 
-function meow_change.change(meownatica, change)
+function meow_change.change(meownatica)
     if meownatica ~= false then
-        local index = (point2 - 1) % reader.len() + 1
+        local index = (meow_point - 1) % reader.len() + 1
         if reader.len() > 0 then
-            local source1 = reader.sys_get('savepath') .. reader.schem(index)
-            if reader.schem(index):find(FORMAT) then
-                local doc = save_u.read(source1)
-                point2 = point2 + 1
+            meow_point = meow_point + 1
+            local source = reader.sys_get('savepath') .. reader.schem(index)
+            local is_exists = file.exists(source)
+            if reader.schem(index):find(FORMAT) and is_exists then
+                local doc = save_u.read(source)
                 container.send_g(save_u.convert_read(doc))
                 return container.get_g(), 0, reader.schem(index)
                 -----------------------------------------------------
-            else
-                point2 = point2 + 1
+            elseif is_exists then
                 return 'convert', reader.schem(index), reader.schem(index)
             end
+            container.send_g({})
+            return {}, 0, 0
         else
             container.send_g({})
             return container.get_g(), 0, 0
         end
-
-    elseif meownatica == false and change == false then
-        local index = (point2 - 1) % reader.len() + 1
-        local source1 = reader.sys_get('savepath')  .. reader.schem(index)
-        if reader.schem(index):find(FORMAT) then
-            local doc = save_u.read(source1)
-            container.send_g(save_u.convert_read(doc))
-            return container.get_g(), 0, reader.schem(index)
-            -----------------------------------------------------
-        else
-            return 'convert', reader.schem(index), reader.schem(index)
-        end
     else
         local index = 1
-        point2 = 2
-        local source1 = ''
+        meow_point = 2
+        local source = ''
         if reader.len() > 0 then
-            source1 = reader.sys_get('savepath') .. reader.schem(index)
+            source = reader.sys_get('savepath') .. reader.schem(index)
         else
             container.send_g({})
             return {}
         end
-        if reader.schem(index):find(FORMAT) then
-            local doc = save_u.read(source1)
+
+        local is_exists = file.exists(source)
+
+        if reader.schem(index):find(FORMAT) and is_exists then
+            local doc = save_u.read(source)
             container.send_g(save_u.convert_read(doc))
             return container.get_g(), 0, reader.schem(index)
             -----------------------------------------------------
-        else
+        elseif is_exists then
             return 'convert', reader.schem(index), reader.schem(index)
         end
+        container.send_g({})
+        return {}, 0, 0
     end
 end
 
